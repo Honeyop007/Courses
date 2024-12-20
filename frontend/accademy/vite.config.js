03401eae7ea2fe049c498db3447c8667f326a04d
Abdul Hanan <abdulhana03@gmail.com>
2024-12-20 22:07:20 +0500

    dist

--- frontend/accademy/vite.config.js
+++ frontend/accademy/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // The backend server
+        changeOrigin: true, // Needed for virtual hosted sites
+        secure: false, // Use this if you're using HTTPS
+        rewrite: (path) => path.replace(/^\/api/, '') // Optional: Remove '/api' prefix if needed
+      }
+    }
+  }
 })
